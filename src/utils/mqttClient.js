29ed1fdd8fbb43db920bd08bf23624f7c322de05
qilinxie02 <qxie@gicw.org>
2024-05-24 13:58:32 -0700

    added mqtt intergration

---
+++ src/utils/mqttClient.js
@@ -0,0 +1,37 @@
+const mqtt = require('mqtt');
+require('dotenv').config();
+
+const brokerUrl = process.env.MQTT_BROKER_URL;
+const options = {
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+};
+
+const client = mqtt.connect(brokerUrl, options);
+
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+});
+
+client.on('error', (err) => {
+  console.error('MQTT connection error:', err);
+});
+
+client.on('message', (topic, message) => {
+  console.log(`Received message from ${topic}: ${message.toString()}`);
+});
+
+const publishMessage = (topic, message) => {
+  client.publish(topic, message, (err) => {
+    if (err) {
+      console.error('Publish error:', err);
+    } else {
+      console.log(`Message published to ${topic}: ${message}`);
+    }
+  });
+};
+
+module.exports = {
+  client,
+  publishMessage,
+};
